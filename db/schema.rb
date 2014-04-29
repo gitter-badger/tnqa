312a58dda51f1d7906163b22e8e23fe14c24c058
Ekaterina Shemerey <rooh@mail.ru>
2014-04-29 20:40:57 +0400

    belong_to user for question

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429155041) do
+ActiveRecord::Schema.define(version: 20140429163540) do
+
+  create_table "questions", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
