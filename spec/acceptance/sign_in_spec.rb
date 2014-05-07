6c7c3eccc3b8f26719c1f70572a609faa97979db
Ekaterina Shemerey <rooh@mail.ru>
2014-05-07 19:03:16 +0400

    add sign_in_spec

---
+++ spec/acceptance/sign_in_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+feature 'Signing in', 'log in to ask' do
+
+  scenario 'Existing user try to log in' do
+    user = create(:user)
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'foobarrr'
+    click_on 'Sign in'
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Existing user try to log in with invalid password' do
+    user = create(:user)
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'invalidpassword'
+    click_on 'Sign in'
+    expect(page).to have_content 'Invalid email or password.'
+  end
+
+  scenario 'Non-existing user try to log in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'invalid@user.com'
+    fill_in 'Password', with: 'invalidpassword'
+    click_on 'Sign in'
+    expect(page).to have_content 'Invalid email or password.'
+  end
+end
