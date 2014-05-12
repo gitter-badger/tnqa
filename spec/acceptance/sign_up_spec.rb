dd3f82ee99c0a5c552a266cf0485f5abec857b69
Ekaterina Shemerey <rooh@mail.ru>
2014-05-12 15:28:34 +0400

    sign up spec refactoring

--- spec/acceptance/sign_up_spec.rb
+++ spec/acceptance/sign_up_spec.rb
@@ -2,29 +2,59 @@ require 'spec_helper'
 
 feature 'Signing up', 'sign up to ask' do
 
-  context "registration" do
-    scenario "user can sign up through registration page" do
-      visit new_user_registration_path
-      fill_in 'Name', with: 'Name'
-      fill_in 'Email', with: 'test@test.ru'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-      click_button "Sign up"
-      expect(page).to have_content "Welcome! You have signed up successfully"
-      expect { post :create, user: attributes_for(:user) }.to change(User, :count).by(1)
-      expect(response).to redirect_to questions_path
-    end
-    scenario "user can not sign up with invalid attributes" do
-      visit new_user_registration_path
-      fill_in 'Name', with: 'Name'
-      fill_in 'Email', with: 'test.ru'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password'
-      click_button "Sign up"
-      expect(page).to have_content "Error"
-      expect { post :create, user: attributes_for(:user) }.to_not change(User, :count)
+  let(:user) {  create :user }
+
+  describe "registration" do
+
+    context "valid attributes" do
+      scenario "user can sign up through registration page" do
+        visit new_user_registration_path
+        sign_up(user)
+        expect(page).to have_content "Welcome! You have signed up successfully"
+        #expect(response).to redirect_to questions_path
+      end
+
+      scenario 'new user is created' do
+        expect do
+          visit new_user_registration_path
+          sign_up(user)
+        end.to change(User, :count).by(1)
+      end
+
+      scenario 'new user is created' do
+          visit new_user_registration_path
+          sign_up(user)
+          expect(current_url).to eq(users_url)
+      end
+
     end
 
+    context "invalid attributes" do
+      scenario "user can not sign up with invalid attributes" do
+        visit new_user_registration_path
+        not_sign_up(user)
+        expect(page).to have_content "Email is invalid"
+      end
+
+      scenario 'new user is not created' do
+      	expect do
+        visit new_user_registration_path
+        not_sign_up(user)
+      end.to_not change(User, :count)
+    end
   end
 
+  scenario 'existing user try to sign in' do
+
+    visit new_user_registration_path
+    fill_in 'Name', with: user.name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content "prohibited this user from being saved"
+  end
+
+end
 end
--- spec/support/acceptance_macros.rb
+++ spec/support/acceptance_macros.rb
@@ -1,10 +1,26 @@
 module AcceptanceMacros
 
-	def sign_in(user)
-		visit new_user_session_path
+  def sign_in(user)
+    visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_on 'Sign in'
-	end
+  end
+
+  def sign_up(user)
+    fill_in 'Name', with: 'Name'
+    fill_in 'Email', with: 'test.ru'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button "Sign up"
+  end
+
+  def not_sign_up(user)
+        fill_in 'Name', with: 'Name'
+        fill_in 'Email', with: 'test.ru'
+        fill_in 'Password', with: 'password'
+        fill_in 'Password confirmation', with: 'password'
+        click_button "Sign up"
+  end
 
 end
