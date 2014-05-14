1898587d88849f750152b6949bc1731cc4bedd8c
Ekaterina Shemerey <rooh@mail.ru>
2014-05-15 01:41:31 +0400

    helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,9 +46,13 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+
+config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each, js: true) do
@@ -62,6 +66,5 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-
   
 end
